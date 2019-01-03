efd7ddf7829844e42defce46e6ce05b3624b7f4b
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-01-03 08:14:00 +0100

    First commit

---
+++ Bil/Controllers/BilController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bil.Controllers
+{
+    public class BilController : Controller
+    {
+        // GET: Bil
+        public ActionResult Index()
+        {
+            var model = new ViewModels.BilIndexViewModel();
+            var db = new Models.DBContext();
+
+            model.Cars.AddRange( db.GetAll().Select(r=>new ViewModels.BilIndexViewModel.BilListViewModel
+            {
+                Manufacturer = r.Manufacturer,
+                Model = r.Model,
+                Year = r.Year,
+                Id = r.Id
+            }) );
+
+            return View(model);
+        }
+
+        public ActionResult View(int id)
+        {
+            var db = new Models.DBContext();
+            var bil = db.Get(id);
+            var model = new ViewModels.BilViewViewModel
+            {
+                Color = bil.Color,
+                Manufacturer = bil.Manufacturer,
+                Model = bil.Model,
+                Price = bil.Price,
+                Year = bil.Year
+            };
+            
+            return View(model);
+        }
+    }
+}
---
+++ Bil/ViewModels/BilIndexViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bil.ViewModels
+{
+    public class BilIndexViewModel
+    {
+        public BilIndexViewModel()
+        {
+            Cars = new List<BilListViewModel>();
+        }
+        public class BilListViewModel
+        {
+            public int Id { get; set; }
+            public string Manufacturer { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+        }
+        public List<BilListViewModel> Cars { get; set; }
+    }
+}
